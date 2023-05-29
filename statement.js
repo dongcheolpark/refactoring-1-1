100bc10257a197c6b528fc00b443837f23de3a33
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2023-05-29 06:05:01 +0000

    함수를 추출합니다.

--- statement.js
+++ statement.js
@@ -45,16 +45,22 @@ export function statement(invoice, plays) {
 		return volumeCredits;
 	}
 
+	const totalVolumeCredits = () => {
+		let volumeCredits = 0;
+		for (let perf of invoice.performances) {
+			volumeCredits += volumeCreditsFor(perf);
+		}
+		return volumeCredits
+	}
+
 	for (let perf of invoice.performances) {
 		// 청구 내역을 출력한다.
 		result += `${playFor(perf).name}: ${usd(amountFor(perf))} ${perf.audience}석\n`;
 		totalAmount += amountFor(perf);
 	}
 
-	let volumeCredits = 0;
-	for (let perf of invoice.performances) {
-		volumeCredits += volumeCreditsFor(perf);
-	}
+	let volumeCredits = totalVolumeCredits();
+
 	result += `총액 ${usd(totalAmount)}\n`;
 	result += `적립 포인트 ${volumeCredits}점\n`;
 
