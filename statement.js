21969d001c65c396b71973038fd305eb5a4678fd
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2023-05-29 05:17:10 +0000

    필요없어진 매개변수를제거합니다.

--- statement.js
+++ statement.js
@@ -9,7 +9,7 @@ export function statement(invoice, plays) {
 		return plays[aPerformance.playID];
 	}
 
-	const amountFor = (aPerformance, play) => {
+	const amountFor = (aPerformance) => {
 		let result = 0;
 		switch (playFor(aPerformance).type) {
 			case 'tragedy':
@@ -35,7 +35,7 @@ export function statement(invoice, plays) {
 	}
 
 	for (let perf of invoice.performances) {
-		let thisAmount = amountFor(perf, playFor(perf));
+		let thisAmount = amountFor(perf);
 
 		// 포인트를 적립한다.
 		volumeCredits += Math.max(perf.audience - 30, 0);
