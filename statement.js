4961eb6a3b0924271f07e01c879306a4bc834815
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2023-05-30 12:00:27 +0000

    변수 이름을 변경합니다.

--- statement.js
+++ statement.js
@@ -53,11 +53,11 @@ export function statement(invoice, plays) {
 	}
 
 	const totalAmount = () => {
-		let totalAmount = 0;
+		let result = 0;
 		for (let perf of invoice.performances) {
-			totalAmount += amountFor(perf);
+			result += amountFor(perf);
 		}
-		return totalAmount
+		return result
 	}
 
 	for (let perf of invoice.performances) {
